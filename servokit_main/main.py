2493404c165536019c7017c4b7dd3925ff57b93f
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-06-05 11:33:49 +0900

    Update main.py

--- servokit_main/main.py
+++ servokit_main/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time    
+import RPi.GPIO as GPIO
 from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
 import movement as mv
 
@@ -14,16 +15,32 @@ MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180,
 
 pca = ServoKit(channels=16)
 
-# function init 
 def init():
     for i in range(numServo):
         pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
 
 
 def main():
-    mv.pcaScenario()
-    
-
+    try:
+        while True:
+            var = raw_input(" Enter direction (f/b/l/r/d) :")
+            if var == 'f':
+                for i in range(5):
+                    mv.forward()
+                    i++1
+            elif var == 'b':
+                for i in range(5):
+                    mv.backward()
+                    i++1
+            elif var == 'l':
+                mv.left_turn()
+            elif var == 'r':
+                mv.right_turn()
+            elif var == 'd':
+                mv.pcaScenario()
+            else:
+                print("Statement Error")
+                
 
 if __name__ == '__main__':
     init()
