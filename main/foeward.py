faa43d71f030b0813044319d45b6a9dc0a902630
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-06-12 20:12:37 +0900

    Add files via upload

---
+++ main/foeward.py
@@ -0,0 +1,73 @@
+# set all servos to initial position
+import time
+
+from board import SCL, SDA
+import busio
+
+# Import the PCA9685 module. Available in the bundle and here:
+#   https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+i2c = busio.I2C(SCL, SDA)
+
+pca1 = PCA9685(i2c)
+
+pca1.frequency = 50
+
+# for i in range(8):
+#     servo[i] = servo.Servo(pca.channels[i])
+servo0 = servo.Servo(pca1.channels[0])
+servo1 = servo.Servo(pca1.channels[1])
+servo2 = servo.Servo(pca1.channels[2])
+servo3 = servo.Servo(pca1.channels[3])
+servo4 = servo.Servo(pca1.channels[4])
+servo5 = servo.Servo(pca1.channels[5])
+servo6 = servo.Servo(pca1.channels[6])
+servo7 = servo.Servo(pca1.channels[7])
+servo8 = servo.Servo(pca1.channels[8])
+servo9 = servo.Servo(pca1.channels[9])
+servo10 = servo.Servo(pca1.channels[10])
+servo11 = servo.Servo(pca1.channels[11])
+
+def returnInit():
+    servo0.angle = 90
+    servo1.angle = 80
+    servo2.angle = 100
+    servo3.angle = 90
+    servo4.angle = 100
+    servo5.angle = 100
+    servo6.angle = 90
+    servo7.angle = 90
+    servo8.angle = 80
+    servo9.angle = 90
+    servo10.angle = 90
+    servo11.angle = 80
+    
+returnInit()
+servo7.angle = 50 
+servo10.angle = 60
+time.sleep(0.3)
+servo1.angle =  110
+servo4.angle = 110
+time.sleep(0.3)
+servo4.angle = 160
+servo1.angle =  120
+servo0.angle = 70
+servo3.angle = 120
+
+
+servo0.angle = 90
+servo1.angle = 120
+servo2.angle = 100
+servo3.angle = 90
+servo4.angle = 100
+servo5.angle = 100
+servo6.angle = 110 # 90 110
+servo7.angle = 50 #90 50
+servo8.angle = 80
+servo9.angle = 90
+servo10.angle = 90# 60 90 110
+servo11.angle = 80
+
+pca1.deinit()
