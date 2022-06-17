7423751cc63c389a39f3c6bd9cd6009c3c16af52
signalho <dlghrnem@yonsei.ac.kr>
2022-06-18 02:09:01 +0900

    multi thread servo ctrl / 2types need exp.

--- main/individualActions/returnInitial.py
+++ main/individualActions/returnInitial.py
@@ -5,6 +5,7 @@ import busio
 import drivers
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
+import threading
 
 i2c = busio.I2C(SCL, SDA)
 pca1 = PCA9685(i2c)
@@ -54,6 +55,78 @@ LB_KNEE_INIT     =           90
 LB_SHOULDER_INIT =           65
 LB_HIP_INIT      =           83
 
+
+'''
+class SpotServo:
+    def __init__(self, servoPin[3]):
+        self.servoPin[3]   = servoPin[3]
+        self.servoKNEE     = servo.Servo(pca1.channels[self.servoPin[0]])
+        self.servoSHLDR    = servo.Servo(pca1.channels[self.servoPin[1]])
+        self.servoHIP      = servo.Servo(pca1.channels[self.servoPin[2]])
+        
+    def _set_servo(self, position[3]):
+        for i in range(3):
+            self.servoKNEE.angle  = position[i]
+            self.servoSHLDR.angle = position[i]
+            self.servoHIP.angle   = position[i]
+            
+def init_spot():
+    RF_Servo = SpotServo([RF_KNEE_PIN, RF_SHOULDER_PIN, RF_HIP_PIN])
+    RB_Servo = SpotServo([RB_KNEE_PIN, RB_SHOULDER_PIN, RB_HIP_PIN])
+    LF_Servo = SpotServo([LF_KNEE_PIN, LF_SHOULDER_PIN, LF_HIP_PIN])
+    LB_Servo = SpotServo([LB_KNEE_PIN, LB_SHOULDER_PIN, LB_HIP_PIN])
+    time.sleep(0.1)
+    display.lcd_clear()   
+    display.lcd_display_string("Initialized")
+
+def SPOT_THREAD_INIT():
+    RF = threading.Thread(target=RF_Servo._set_servo, args=([RF_KNEE_INIT, RF_SHOULDER_INIT, RF_HIP_INIT]))
+    RB = threading.Thread(target=RB_Servo._set_servo, args=([RB_KNEE_INIT, RB_SHOULDER_INIT, RB_HIP_INIT]))
+    LF = threading.Thread(target=LF_Servo._set_servo, args=([LF_KNEE_INIT, LF_SHOULDER_INIT, LF_HIP_INIT]))
+    LB = threading.Thread(target=LB_Servo._set_servo, args=([LB_KNEE_INIT, LB_SHOULDER_INIT, LB_HIP_INIT]))
+    RF.start()
+    RB.start()
+    LF.start()
+    LB.start()
+
+    # NOT SURE IF THIS WORKS SIMULANEOUSLY
+    # RF = threading.Thread(target=RF_Servo._set_servo, args=([RF_KNEE_INIT, RF_SHOULDER_INIT, RF_HIP_INIT])).start()
+    # RB = threading.Thread(target=RB_Servo._set_servo, args=([RB_KNEE_INIT, RB_SHOULDER_INIT, RB_HIP_INIT])).start()
+    # LF = threading.Thread(target=LF_Servo._set_servo, args=([LF_KNEE_INIT, LF_SHOULDER_INIT, LF_HIP_INIT])).start()
+    # LB = threading.Thread(target=LB_Servo._set_servo, args=([LB_KNEE_INIT, LB_SHOULDER_INIT, LB_HIP_INIT])).start()
+    # WAIT FOR THREAD TO END
+    # RF.join()
+    # RB.join()
+    # LF.join()
+    # LB.join()
+
+def ctrl_spot(angle[12]):
+    RF = threading.Thread(target=RF_Servo._set_servo, args=([angle[0], angle[1], angle[2]]))
+    RB = threading.Thread(target=RB_Servo._set_servo, args=([angle[3], angle[4], angle[5]]))
+    LF = threading.Thread(target=LF_Servo._set_servo, args=([angle[6], angle[7], angle[8]]))
+    LB = threading.Thread(target=LB_Servo._set_servo, args=([angle[9], angle[10], angle[12]]))
+    RF.start()
+    RB.start()
+    LF.start()
+    LB.start()
+
+    # NOT SURE IF THIS WORKS SIMULANEOUSLY
+    # RF = threading.Thread(target=RF_Servo._set_servo, args=([angle[0], angle[1], angle[2]])).start()
+    # RB = threading.Thread(target=RB_Servo._set_servo, args=([angle[3], angle[4], angle[5]])).start()
+    # LF = threading.Thread(target=LF_Servo._set_servo, args=([angle[6], angle[7], angle[8]])).start()
+    # LB = threading.Thread(target=LB_Servo._set_servo, args=([angle[9], angle[10], angle[12]])).start()
+    # WAIT FOR THREAD TO END
+    # RF.join()
+    # RB.join()
+    # LF.join()
+    # LB.join()
+    
+# example
+# ctrl_spot([10,10,10,10,10,10,10,10,10,10,10,10])
+'''
+
+
+
 RF_KNEE          =           servo.Servo(pca1.channels[RF_KNEE_PIN])
 RF_SHOULDER      =           servo.Servo(pca1.channels[RF_SHOULDER_PIN])
 RF_HIP           =           servo.Servo(pca1.channels[RF_HIP_PIN])
@@ -66,9 +139,6 @@ LF_HIP           =           servo.Servo(pca1.channels[LF_HIP_PIN])
 LB_KNEE          =           servo.Servo(pca1.channels[LB_KNEE_PIN])
 LB_SHOULDER      =           servo.Servo(pca1.channels[LB_SHOULDER_PIN])
 LB_HIP           =           servo.Servo(pca1.channels[LB_HIP_PIN])
-
-CTRL_INIT        =           [12] 
-CTRL_ANGLE       =           [12]
 
 def init_spot():
     RF_KNEE.angle     = RF_KNEE_INIT
@@ -87,8 +157,53 @@ def init_spot():
     display.lcd_clear()   
     display.lcd_display_string("Initialized")
 
+def ctrl_RF(RF_KNEE_ANG, RF_SHOULDER_ANG, RF_HIP_ANG):
+    RF_KNEE.angle = RF_KNEE_ANG
+    RF_SHOULDER.angle = RF_SHOULDER_ANG
+    RF_HIP.angle = RF_HIP_ANG
+    print("ctrl_RF")
+
+def ctrl_RB(RB_KNEE_ANG, RB_SHOULDER_ANG, RB_HIP_ANG):
+    RB_KNEE.angle = RB_KNEE_ANG
+    RB_SHOULDER.angle = RB_SHOULDER_ANG
+    RB_HIP.angle = RB_HIP_ANG
+    print("ctrl_RB")
+
+def ctrl_LF(LF_KNEE_ANG, LF_SHOULDER_ANG, LF_HIP_ANG):
+    LF_KNEE.angle = LF_KNEE_ANG
+    LF_SHOULDER.angle = LF_SHOULDER_ANG
+    LF_HIP.angle = LF_HIP_ANG
+    print("ctrl_LF")
+
+def ctrl_LB(LB_KNEE_ANG, LB_SHOULDER_ANG, LB_HIP_ANG):
+    LB_KNEE.angle = LB_KNEE_ANG
+    LB_SHOULDER.angle = LB_SHOULDER_ANG
+    LB_HIP.angle = LB_HIP_ANG
+    print("ctrl_LB")
+
+def SPOT_THREAD_INIT():
+    RF = threading.Thread(target=ctrl_RF, args=(RF_KNEE_INIT, RF_SHOULDER_INIT, RF_HIP_INIT))
+    RB = threading.Thread(target=ctrl_RB, args=(RB_KNEE_INIT, RB_SHOULDER_INIT, RB_HIP_INIT))
+    LF = threading.Thread(target=ctrl_LF, args=(LF_KNEE_INIT, LF_SHOULDER_INIT, LF_HIP_INIT))
+    LB = threading.Thread(target=ctrl_LB, args=(LB_KNEE_INIT, LB_SHOULDER_INIT, LB_HIP_INIT))
+
+    RF.start()
+    RB.start()
+    LF.start()
+    LB.start()
+
+    RF.join()
+    RB.join()
+    LF.join()
+    LB.join()
+
+    print("SPOT_THREAD_INIT")
+
+
+
 
 def main():
+    SPOT_THREAD_INIT()
     display.lcd_display_string("   INIT POSE   ", 1) 
     init_spot()
     pca1.deinit()
@@ -97,87 +212,34 @@ def main():
 if __name__ == '__main__':
     main()
 
+
+
+
+
+
+
 '''
-import time
-
-class Hardware:
-    def __init__(self):
-        self.N = 5
-        self.servo_pos = [0,0,0,0,0]
-        self.kit = .... initialization of driver
-        
-    def _set_servo(self, servo, position):
-        """ set single servo [0...N-1] to a position"""
-        # print("servo", servo, "to", position)
-        self.kit.servo[servo].angle = position
-        
-    def target_position(self, p0, p1, p2, p3, p4, delay=0):
-        """set all servo to a position which needs to be reached after delay second"""
-            
-        new_pos =  [p0, p1, p2, p3, p4]
-        if delay < 0.02:
-            # set all servo immediately to position
-            for k in range(self.N):
-                # set servo position for servo 'k'
-                self._set_servo(k, new_pos[k] )
-        else:
-            ddelay = 0.02 # 20 ms are servo period time
-            iterations = int( delay / ddelay)  
-            # if number of iterations is too high, then decrease time resolution a bit
-            if iterations > 32:
-                ddelay = 0.04
-                iterations = int( delay / ddelay) 
-                
-            for i in range(iterations):
-                for k in range(self.N):
-                    # set servo position for servo 'k'
-                    pos = (new_pos[k] - self.servo_pos[k]) / iterations * i
-                    self._set_servo(k, pos)
-                
-                time.sleep(ddelay)
-        
-        self.servo_pos = new_pos
-        
-# example usage
-
-hardware = Hardware()
-hardware.target_position(10, -20, 22, -60, 100, delay=10.0)
-
-
-
-
-
-
-
-
-from adafruit_servokit import ServoKit
-import threading
-import time
-kit = ServoKit(channels=16)
-
-def thumbFinger(fingerAngle, fingerDelay):
-    for fingerAngle in range (0,180):
-        kit.servo[1].angle = fingerAngle
-        time.sleep(fingerDelay)
-        print(fingerAngle, fingerDelay)
-
-def indexFinger(fingerAngle, fingerDelay):
-    for fingerAngle in range (0,180):
-        kit.servo[2].angle = fingerAngle
-        time.sleep(fingerDelay)
-        print(fingerAngle, fingerDelay)
-
-
-if __name__ == "__main__":
-    t1 = threading.Thread(target=thumbFinger, args=(180,0.01))
-    t2 = threading.Thread(target=indexFinger, args=(180,0.01))
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    print("Done!")
+fingers = [] # empty list of fingers
+for i in range(5):
+	t = Hardware() # create a finger
+	fingers.append(t) # add it to the list
+# you now have 5 fingers
 '''
-###
+
+'''
+def do_something():
+    print('Sleeping 1 seconds')
+    time.sleep(1)
+    print('Done Sleeping...')
+ 
+if __name__ == '__main__':
+
+    threads = []
+    for _ in range(10): # 10 reps
+        t = threading.Thread(target=do_something)
+        t.start()
+        threads.append(t)
+        
+    for thread in threads:
+        thread.join()
+'''
