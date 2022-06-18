df03638883a7c648225418570b4078aeadc510c9
wkdalswns0427 <wkdalswns0427@yonsei.ac.kr>
2022-06-19 03:21:11 +0900

    new_target_fun

--- main/individualActions/slow_increment.py
+++ main/individualActions/slow_increment.py
@@ -5,169 +5,144 @@ import busio
 import drivers
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
-import multiprocessing 
 import threading
-import math
-
 
 i2c = busio.I2C(SCL, SDA)
 pca1 = PCA9685(i2c)
 pca1.frequency = 60
 display = drivers.Lcd()
 
-#########################################################################
-# Right front knee        CHANNEL NUM : 0         initial position: 80
-# Right front shoulder    CHANNEL NUM : 1         initial position: 80
-# Right front hip         CHANNEL NUM : 2         initial position: 90
-# Right back knee         CHANNEL NUM : 4         initial position: 110
-# Right back shoulder     CHANNEL NUM : 5         initial position: 100
-# Right back hip          CHANNEL NUM : 6         initial position: 90
-# Left back knee          CHANNEL NUM : 8         initial position: 90
-# Left back shoulder      CHANNEL NUM : 9         initial position: 100
-# Left back hip           CHANNEL NUM : 10        initial position: 85
-# Left front knee         CHANNEL NUM : 12        initial position: 90
-# Left front shoulder     CHANNEL NUM : 13        initial position: 65
-# left front hip          CHANNEL NUM : 14        initial position: 83
-RF_KNEE_INIT     =           80
-RF_SHOULDER_INIT =           90
-RF_HIP_INIT      =           90
-RB_KNEE_INIT     =           85
-RB_SHOULDER_INIT =           70
-RB_HIP_INIT      =           90
-LF_KNEE_INIT     =           110
-LF_SHOULDER_INIT =           100
-LF_HIP_INIT      =           85
-LB_KNEE_INIT     =           90
-LB_SHOULDER_INIT =           80
-LB_HIP_INIT      =           88
-#########################################################################
+#################### you do not cange these ############################
 MIN_ANGLE        =           0
 MAX_ANGLE        =           180
 
 RF_KNEE_PIN      =           0
 RF_SHOULDER_PIN  =           1
 RF_HIP_PIN       =           2
-RB_KNEE_PIN      =           12
-RB_SHOULDER_PIN  =           9
-RB_HIP_PIN       =           10
-LF_KNEE_PIN      =           4
-LF_SHOULDER_PIN  =           5
-LF_HIP_PIN       =           6
-LB_KNEE_PIN      =           8
+RB_KNEE_PIN      =           4
+RB_SHOULDER_PIN  =           5
+RB_HIP_PIN       =           6
+LF_KNEE_PIN      =           8
+LF_SHOULDER_PIN  =           9
+LF_HIP_PIN       =           10
+LB_KNEE_PIN      =           12
 LB_SHOULDER_PIN  =           13
 LB_HIP_PIN       =           14
 
-RF_KNEE          =           servo.Servo(pca1.channels[RF_KNEE_PIN])
-RF_SHOULDER      =           servo.Servo(pca1.channels[RF_SHOULDER_PIN])
-RF_HIP           =           servo.Servo(pca1.channels[RF_HIP_PIN])
-RB_KNEE          =           servo.Servo(pca1.channels[RB_KNEE_PIN])
-RB_SHOULDER      =           servo.Servo(pca1.channels[RB_SHOULDER_PIN])
-RB_HIP           =           servo.Servo(pca1.channels[RB_HIP_PIN])
-LF_KNEE          =           servo.Servo(pca1.channels[LF_KNEE_PIN])
-LF_SHOULDER      =           servo.Servo(pca1.channels[LF_SHOULDER_PIN])
-LF_HIP           =           servo.Servo(pca1.channels[LF_HIP_PIN])
-LB_KNEE          =           servo.Servo(pca1.channels[LB_KNEE_PIN])
-LB_SHOULDER      =           servo.Servo(pca1.channels[LB_SHOULDER_PIN])
-LB_HIP           =           servo.Servo(pca1.channels[LB_HIP_PIN])
+RF_LEG           =   [RF_KNEE_PIN, RF_SHOULDER_PIN, RF_HIP_PIN]
+RB_LEG           =   [RB_KNEE_PIN, RB_SHOULDER_PIN, RB_HIP_PIN]
+LF_LEG           =   [LF_KNEE_PIN, LF_SHOULDER_PIN, LF_HIP_PIN]
+LB_LEG           =   [LB_KNEE_PIN, LB_SHOULDER_PIN, LB_HIP_PIN]
 
-def init_spot():
-    RF_KNEE.angle     = RF_KNEE_INIT
-    RF_SHOULDER.angle = RF_SHOULDER_INIT
-    RF_HIP.angle      = RF_HIP_INIT
-    RB_KNEE.angle     = RB_KNEE_INIT
-    RB_SHOULDER.angle = RB_SHOULDER_INIT
-    RB_HIP.angle      = RB_HIP_INIT
-    LF_KNEE.angle     = LF_KNEE_INIT
-    LF_SHOULDER.angle = LF_SHOULDER_INIT
-    LF_HIP.angle      = LF_HIP_INIT
-    LB_KNEE.angle     = LB_KNEE_INIT
-    LB_SHOULDER.angle = LB_SHOULDER_INIT
-    LB_HIP.angle      = LB_HIP_INIT
-    time.sleep(0.1)
-    # display.lcd_clear()   
-    # display.lcd_display_string("Initialized",1)
+RF_KNEE_INIT     =           80
+RF_SHOULDER_INIT =           80
+RF_HIP_INIT      =           90
+RB_KNEE_INIT     =           110
+RB_SHOULDER_INIT =           100
+RB_HIP_INIT      =           90
+LF_KNEE_INIT     =           90
+LF_SHOULDER_INIT =           100
+LF_HIP_INIT      =           85
+LB_KNEE_INIT     =           90
+LB_SHOULDER_INIT =           65
+LB_HIP_INIT      =           83
+######################################################################
 
-def slow_offset(prev_angle, post_angle, servo, step = 1):
-    offset = abs(prev_angle - post_angle)
+class SpotServo:
+    def __init__(self, servoPin[3]):
+        self.servoPin[3]   = servoPin[3]
+        self.servoPos[3]   = [0, 0, 0]
+        self.servoKNEE     = servo.Servo(pca1.channels[self.servoPin[0]])
+        self.servoSHLDR    = servo.Servo(pca1.channels[self.servoPin[1]])
+        self.servoHIP      = servo.Servo(pca1.channels[self.servoPin[2]])
+        
+    def _set_servo(self, position[3]):
+        self.servoKNEE.angle  = position[0]
+        self.servoSHLDR.angle = position[1]
+        self.servoHIP.angle   = position[2]
     
-    if(prev_angle < post_angle):
-        for i in range(0, offset, step):
-            servo.angle(prev_angle+i)
-    elif(prev_angle > post_angle):
-        for i in range(0, offset, step):
-            servo.angle(prev_angle-i)
-    else:
-        display.lcd_display_string("I do not move!", 1)
+    # 3 servo 
+    def target_position(self, prev_angle[3], post_angle[3],  step = 1):
+        offset = [0,0,0]
+        diff = [0,0,0]
+        addup = [0,0,0]
+        cnt = [0,0,0]
+        abscnt = 1
+        
+        for i in range(0,2):
+            diff[i] = prev_angle[i] - post_angle[i]
+            offset[i] = abs(prev_angle[i] - post_angle[i])
+        
+        for i in range(0,2):
+            if diff[i]>0:
+                addup[i] = -1
+            elif diff[i]<0:
+                addup[i] = 1
+                    
+        while cnt[0]<=offset[0] or cnt[1]<=offset[1] or cnt[2]<=offset[2]:
+            for i in range(0,2):
+                if cnt[i]<=offset[i]:
+                    addup[i] = addup[i]*abscnt
+                else:
+                    addup[i] = 0
+
+            self.servoKNEE.angle  = prev_angle[0] + addup[0]
+            self.servoSHLDR.angle = prev_angle[1] + addup[1]
+            self.servoHIP.angle   = prev_angle[2] + addup[2]
+            abscnt += 1
             
-
-
-def ctrl_RF(lock, RF_KNEE_ANG, RF_SHOULDER_ANG, RF_HIP_ANG):
-    RF_KNEE.angle = RF_KNEE_ANG
-    RF_SHOULDER.angle = RF_SHOULDER_ANG
-    RF_HIP.angle = RF_HIP_ANG
-    print("ctrl_RF")
-
-def ctrl_RB(RB_KNEE_ANG, RB_SHOULDER_ANG, RB_HIP_ANG):
-    RB_KNEE.angle = RB_KNEE_ANG
-    RB_SHOULDER.angle = RB_SHOULDER_ANG
-    RB_HIP.angle = RB_HIP_ANG
-    print("ctrl_RB")
-
-def ctrl_LF(LF_KNEE_ANG, LF_SHOULDER_ANG, LF_HIP_ANG):
-    LF_KNEE.angle = LF_KNEE_ANG
-    LF_SHOULDER.angle = LF_SHOULDER_ANG
-    LF_HIP.angle = LF_HIP_ANG
-    print("ctrl_LF")
-
-def ctrl_LB(LB_KNEE_ANG, LB_SHOULDER_ANG, LB_HIP_ANG):
-    LB_KNEE.angle = LB_KNEE_ANG
-    LB_SHOULDER.angle = LB_SHOULDER_ANG
-    LB_HIP.angle = LB_HIP_ANG
-    print("ctrl_LB")
-
-def SPOT_THREAD_INIT():
-    lock = multiprocessing.Lock()
-    e = multiprocessing.Event()
-    print("event") 
-    RF = multiprocessing.Process(target=ctrl_RF, args=(lock,RF_KNEE_INIT, RF_SHOULDER_INIT, RF_HIP_INIT))
-    RB = multiprocessing.Process(target=ctrl_RB, args=(RB_KNEE_INIT, RB_SHOULDER_INIT, RB_HIP_INIT))
-    LF = multiprocessing.Process(target=ctrl_LF, args=(LF_KNEE_INIT, LF_SHOULDER_INIT, LF_HIP_INIT))
-    LB = multiprocessing.Process(target=ctrl_LB, args=(LB_KNEE_INIT, LB_SHOULDER_INIT, LB_HIP_INIT))
-    print("event") 
-
+            for i in range(0,2):
+                cnt[i]+=1
     
-    RB.start()
-    print("rb")
+            
+def init_spot():
+    RF_Servo = SpotServo(RF_LEG)
+    RB_Servo = SpotServo(RB_LEG)
+    LF_Servo = SpotServo(LF_LEG)
+    LB_Servo = SpotServo(LB_LEG)
+    time.sleep(0.1)
+    display.lcd_clear()   
+    display.lcd_display_string("Initialized")
+    return RF_Servo, RB_Servo, LF_Servo, LB_Servo
+
+def init_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo):
+    RF = threading.Thread(target=RF_Servo._set_servo, args=([RF_KNEE_INIT, RF_SHOULDER_INIT, RF_HIP_INIT]))
+    RB = threading.Thread(target=RB_Servo._set_servo, args=([RB_KNEE_INIT, RB_SHOULDER_INIT, RB_HIP_INIT]))
+    LF = threading.Thread(target=LF_Servo._set_servo, args=([LF_KNEE_INIT, LF_SHOULDER_INIT, LF_HIP_INIT]))
+    LB = threading.Thread(target=LB_Servo._set_servo, args=([LB_KNEE_INIT, LB_SHOULDER_INIT, LB_HIP_INIT]))
     RF.start()
-    print("rf")
+    RB.start()
     LF.start()
-    print("lf")
     LB.start()
-    print("lb")
 
-    e.set()
+def ctrl_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo, prev_angle[12],post_angle[12]):
+    RF = threading.Thread(target=RF_Servo.target_position, args=([prev_angle[0], prev_angle[1], prev_angle[2]], [post_angle[0], post_angle[1], post_angle[2]]))
+    RB = threading.Thread(target=RB_Servo.target_position, args=([prev_angle[3], prev_angle[4], prev_angle[5]], [post_angle[3], post_angle[4], post_angle[5]]))
+    LF = threading.Thread(target=LF_Servo.target_position, args=([prev_angle[6], prev_angle[7], prev_angle[8]], [post_angle[6], post_angle[7], post_angle[8]]))
+    LB = threading.Thread(target=LB_Servo.target_position, args=([prev_angle[9], prev_angle[10], prev_angle[11]], [post_angle[9], post_angle[10], post_angle[11]]))
+    RF.start()
+    RB.start()
+    LF.start()
+    LB.start()
 
-    # RF.join()
-    # RB.join()
-    # LF.join()
-    # LB.join()
-
-    print("SPOT_THREAD_INIT")
-    return(RF, RB,LF,LB)
+# [RF_Servo, RB_Servo, LF_Servo, LB_Servo] = init_spot()
+# init_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo)
+# target[12] = [90,90,90,90,90,90,90,90,90,90,90,90]
+# ctrl_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo, target[12])
 
 
 
 def main():
-
-    [RF, RB,LF,LB]=SPOT_THREAD_INIT()
+    [RF_Servo, RB_Servo, LF_Servo, LB_Servo] = init_spot()
+    init_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo)
+    target[12] = [90,90,90,90,90,90,90,90,90,90,90,90]
+    ctrl_pos_spot(RF_Servo, RB_Servo, LF_Servo, LB_Servo, target[12])
     display.lcd_display_string("   INIT POSE   ", 1) 
-    init_spot()
     pca1.deinit()
 
 
 if __name__ == '__main__':
     main()
-
 
 
 
